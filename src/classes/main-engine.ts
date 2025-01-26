edb2d83d76548ff5a726818177d7326c7164dfad
Artjom <artjombebenin@gmail.com>
2025-01-26 14:52:30 +0400

    small refactorings

--- src/classes/main-engine.ts
+++ src/classes/main-engine.ts
@@ -1,56 +1,27 @@
 import { Scene, GameObjects, Types, Physics } from 'phaser'
 
-import { IMapTilesIndexes } from '@/types/phaser-types'
-
 import DudeSet from '@assets/dude.png'
-
 import textMapRaw from '@assets/maps/map4.txt?url'
 import tilesRaw from '@assets/kenny_platformer_32.png'
 
-const tileIndexes: IMapTilesIndexes = {
-  '#': 14, 'D': 53, 'DD': 58, 't': 18, 'tt': 23, 'k': 34, 'B': 7, 'w': 4, 'ww': 6, 'T': 33, 'A': 16
-}
+import { MapSceneLevels } from '@/classes/map-scene-levels'
 
 export class MainEngine extends Scene {
   _progress!: GameObjects.Graphics
   _controls!: Phaser.Cameras.Controls.FixedKeyControl
 
-  _groundLayer!: Phaser.Tilemaps.TilemapLayer | null
-  _envLayer!: Phaser.Tilemaps.TilemapLayer | null
-
-  _tileWidth = 32
+  _mapLevels!: MapSceneLevels
 
   constructor(name: string) {
     super(name)
   }
 
   create() {
-    const symbolMap: Array<string> = this.cache.text.get('textMap').split('\n')
-    if (symbolMap[symbolMap.length - 1].length == 0) {
-      symbolMap.splice(symbolMap.length - 1, 1)
-    }
 
-    if (!(symbolMap.length > 0 && symbolMap[0].length > 0)) {
-      console.error('Something wrong with json map, json dont load correctly!')
-      return
-    }
-
-    const map = this.make.tilemap({
-      width: symbolMap[0].length, height: symbolMap.length,
-      tileWidth: this._tileWidth, tileHeight: this._tileWidth
-    })
-    const tileset = map.addTilesetImage('tileSet') as Phaser.Tilemaps.Tileset
-
-    this._groundLayer = this.getLayerForSymbols(['#'], 'groundLayer', map, tileset, symbolMap)
-    this._envLayer = this.getLayerForSymbols(['D', 't', 'k', 'B', 'w', 'T', 'A'], 'env-layer', map, tileset, symbolMap)
-
-    const dude = this.physics.add.sprite(100, 450, 'dude')
-
-    // console.log('grandLayer', this._groundLayer)
+    this._mapLevels = new MapSceneLevels(this, 'textMap', 'tileSet')
 
     this.cameras.main.setScroll(0, 0)
-    this.cameras.main.setBounds(0, 0,
-              symbolMap[0].length * this._tileWidth, symbolMap.length * this._tileWidth)
+    this.cameras.main.setBounds(0, 0, this._mapLevels?.mapWidth, this._mapLevels?.mapHeight)
 
     const cursors = this.input.keyboard?.createCursorKeys()
     if (!cursors) return
@@ -63,6 +34,8 @@ export class MainEngine extends Scene {
       speed: 1
     }
     this._controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig)
+
+    const dude = this.physics.add.sprite(100, 450, 'dude')
   }
 
   update(time: number, delta: number): void {
@@ -87,47 +60,5 @@ export class MainEngine extends Scene {
     this._progress.fillStyle(0xffffdd, 1)
     const gameSize = this.scale.gameSize
     this._progress.fillRect(0, (gameSize.height - 22), gameSize.width * value, 20)
-  }
-
-  private getLayerForSymbols(
-    symbols: string[], nameLayer: string, map: Phaser.Tilemaps.Tilemap, tiles: Phaser.Tilemaps.Tileset, symMap: string[]) {
-
-    const width = symMap[0].length
-    const height = symMap.length
-
-    const notNulls = (strings: string[], i: number, j: number) => {
-      return strings[i] && !(strings[i][j] == undefined || strings[i][j] == null)
-    }
-
-    const indexesMap: Array<Array<number>> = new Array(height)
-    for (let i = 0; i < indexesMap.length; i++) {
-      indexesMap[i] = new Array(width)
-      for (let j = 0; j < indexesMap[i].length; j++) {
-        // orders symbolos for this layer
-        symbols.findIndex((element) => {
-          // check symbol map for null, undefined
-          if (!notNulls(symMap, i, j)) return false
-
-          if (symMap[i][j] == element) {
-            if (notNulls(symMap, i - 1, j) && symMap[i - 1][j] == element && tileIndexes[element + element]) {
-              indexesMap[i][j] = tileIndexes[element + element]
-            } else {
-              indexesMap[i][j] = tileIndexes[element]
-            }
-            // stop search for these symbols
-            return true
-          } else {
-            // empty element maybe next?
-            indexesMap[i][j] = (-1)
-            return false
-          }
-        })
-      }
-    }
-
-    const layer = map.createBlankLayer(nameLayer, tiles)
-    if (!layer) { return null }
-    layer.putTilesAt(indexesMap, 0, 0)
-    return layer
   }
 }
