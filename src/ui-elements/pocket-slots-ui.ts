a2e9ce9189f744897d0e53d8c29399446d979871
Artjom <artjombebenin@gmail.com>
2025-04-12 23:01:54 +0400

    sos-12045 added set settings for slots menu

--- src/ui-elements/pocket-slots-ui.ts
+++ src/ui-elements/pocket-slots-ui.ts
@@ -1,11 +1,11 @@
 import { LitElement, css, html, unsafeCSS } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
-import { PocketItemNull } from '@/types/main-types'
+import { NullOrGameStateSettings, PocketItemNull } from '@/types/main-types'
 
 import { EventBus } from '@/classes/event-bus'
 
-import { BusEventsList } from '@/types/enums'
+import { BusEventsList, GameStateSettings } from '@/types/enums'
 
 import { GameStateElement } from '@/classes/gamestate-element'
 
@@ -16,6 +16,12 @@ import '@/ui-elements/pocket-slots-item-ui'
 
 @customElement('pocket-slots-ui')
 export class PocketSlotsUi extends GameStateElement {
+
+  _stateSettings: NullOrGameStateSettings = [
+    GameStateSettings.pocketItems,
+    GameStateSettings.selectedPocketItem,
+    GameStateSettings.isDudeDropAvailable,
+  ]
 
   render() {
     if (!this._game) return
